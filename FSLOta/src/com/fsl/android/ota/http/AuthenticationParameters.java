afb27cf9daa1cbafdc55601cf23cf2075f358d15
Den <motometalor@gmail.com>
2019-02-12 12:58:03 +0300

    FSLOta: add java classes for https support
    class Api                - wrapper upon URL connection and SSL context.
    AuthenticationParameters - wrapper for certificate and signature.
    ExtendedTrustManager     - keystore and signer helper.
    SSLContextFactory        - context and signatures managed creator.

---
+++ FSLOta/src/com/fsl/android/ota/http/AuthenticationParameters.java
@@ -0,0 +1,50 @@
+package com.fsl.android.ota.http;
+
+import java.io.File;
+
+public class AuthenticationParameters {
+    private File clientCertificate = null;
+    private byte[] p12cert = null;
+    private String clientCertificateAssertName = null;
+    private String clientCertificatePassword = null;
+    private String caCertificate = null;
+
+    public File getClientCertificate() {
+        return clientCertificate;
+    }
+
+    public byte[] getClientCertificateP12Content() {
+        return p12cert;
+    }
+
+    public void setClientCertificateAssertName(String path) {
+        clientCertificateAssertName = path;
+    }
+
+    public String getClientCertificateAssertName() {
+        return clientCertificateAssertName;
+    }
+
+    public void setClientCertificate(File clientCertificate) {
+        this.clientCertificate = clientCertificate;
+    }
+
+    public String getClientCertificatePassword() {
+        return clientCertificatePassword;
+    }
+
+    public void setP12Certificate(byte[] p) {
+        p12cert = p;
+    }
+    public void setClientCertificatePassword(String clientCertificatePassword) {
+        this.clientCertificatePassword = clientCertificatePassword;
+    }
+
+    public String getCaCertificate() {
+        return caCertificate;
+    }
+
+    public void setCaCertificate(String caCertificate) {
+        this.caCertificate = caCertificate;
+    }
+}
